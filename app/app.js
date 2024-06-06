02a11ac54bf98cb6bab6e90156dd54c48cf62a04
Ayelen <ayelen.celis92@gmail.com>
2024-06-05 19:37:44 -0500

    Agregando cors

--- app/app.js
+++ app/app.js
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import express from "express";
 import ruta from "./routes/index.js";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended : true}));
+app.use(cors());
 app.set("port", process.env.PORT);
 app.use("/", ruta);
+
 
 
 
